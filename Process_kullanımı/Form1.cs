d4baede5e491f2ec728c663e857fc3e9729126c9
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-02-25 10:48:49 +0300

    Add project files.

---
+++ Process_kullanımı/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Process_kullanımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("calc.exe");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+    }
+}
